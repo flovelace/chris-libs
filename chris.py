acc838516f3d9e2e834b09250d6fde5332e378d9
fairfax <lovelace.af@gmail.com>
2022-02-04 02:02:58 -0500

    first commit

---
+++ chris.py
@@ -0,0 +1,16 @@
+# for notation in code, use the hash symbol
+
+# create a string variable
+# person = "Chris"
+
+# create the string
+# method 1
+# print("Do mean things to " + person)
+# method 2
+# print("Do mean things to {}".format(person))
+# method 3 (f string)
+# print(f"Do mean things to {person}")
+
+adj = input("Adjective: ")
+
+chrislib = f"Chris is so {adj}."
